26419ee3b9b84dd74650754e8a6e1ee81b700f2f
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-18 17:25:24 +0700

    feat: membuat komponen siswa top app bar

---
+++ app/src/main/java/com/example/dataclass/view/SiswaTopAppBar.kt
@@ -0,0 +1,39 @@
+package com.example.dataclass.view
+
+import android.R.attr.navigationIcon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.dataclass.R
+import java.lang.reflect.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    NavigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(title = { Text(title),
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        })
+}
