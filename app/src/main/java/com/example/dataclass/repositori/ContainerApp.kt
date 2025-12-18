2423c8eb649b1c0eda9c397c2cdbdeca09f4b859
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-18 15:46:09 +0700

    feat: deklarasi base url, http client, dan logging di dala class container app

--- app/src/main/java/com/example/dataclass/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/dataclass/repositori/ContainerApp.kt
@@ -13,3 +13,18 @@ interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor (logging)
+        .build()
+
+
+}
+
+
