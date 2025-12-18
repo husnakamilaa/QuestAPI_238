a7fdc9bf1f558045d3508edc7d99443bb28c2a33
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-18 17:59:05 +0700

    fix: memperbaiki import untuk mengatasi error

--- app/src/main/java/com/example/dataclass/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/dataclass/view/SiswaTopAppBar.kt
@@ -1,7 +1,7 @@
 package com.example.dataclass.view
 
-import android.R.attr.navigationIcon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,10 +9,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.example.dataclass.R
-import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,9 +21,9 @@ fun SiswaTopAppBar(
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
-    NavigateUp: () -> Unit = {}
+    navigateUp: () -> Unit = {}
 ){
-    CenterAlignedTopAppBar(title = { Text(title),
+    CenterAlignedTopAppBar(title = { Text(title)},
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         navigationIcon = {
