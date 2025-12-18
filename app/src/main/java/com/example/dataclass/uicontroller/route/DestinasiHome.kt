efc9f86af6fee10e6c80f74adac3a6d52d542de9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-18 16:08:22 +0700

    chore: deklarasi route untuk halaman home

---
+++ app/src/main/java/com/example/dataclass/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.dataclass.uicontroller.route
+
+import com.example.dataclass.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
