cf451eb89da31db98f5c595e1c688620262c5a2e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-18 15:21:39 +0700

    feat: deklarasi retrofit interface dan api method post untuk menambahkan data siswa

--- app/src/main/java/com/example/dataclass/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/dataclass/apiservice/ServiceApiSiswa.kt
@@ -9,5 +9,6 @@ interface ServiceApiSiswa  {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
