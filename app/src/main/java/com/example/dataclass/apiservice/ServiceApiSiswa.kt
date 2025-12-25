8268807a89ea8f9fd28e814fe496ebeb7c28419f
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-25 15:25:21 +0700

    feat: menambahkan method put untuk edit siswa

--- app/src/main/java/com/example/dataclass/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/dataclass/apiservice/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.dataclass.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa  {
@@ -16,5 +17,6 @@ interface ServiceApiSiswa  {
     @GET("baca1Teman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
-
+    @PUT("eidtTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
