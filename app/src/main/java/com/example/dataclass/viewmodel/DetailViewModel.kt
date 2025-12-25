2382ee785200400051d74b804d2ae72bfd578acc
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-25 17:38:01 +0700

    feat: deklarasi class detail view model

--- app/src/main/java/com/example/dataclass/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/dataclass/viewmodel/DetailViewModel.kt
@@ -21,3 +21,39 @@ sealed interface StatusUIDetail {
     object Error: StatusUIDetail
     object Loading: StatusUIDetail
 }
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init{
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa(){
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if(resp.isSuccessful){
+            println("Sukses Hapus Data: ${resp.message()}")
+        }else{
+            println("Gagal Hapus Data: ${resp.errorBody()}")
+        }
+    }
+}
