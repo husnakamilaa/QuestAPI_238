9b720d4e0d044a1f5ecad21b3d7e2159e8b3aae3
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-25 19:49:11 +0700

    feat: menambahkan function BodyDetailDataSiswa

--- app/src/main/java/com/example/dataclass/view/HalamanDetail.kt
+++ app/src/main/java/com/example/dataclass/view/HalamanDetail.kt
@@ -1,28 +1,48 @@
 package com.example.dataclass.view
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dataclass.R
+import com.example.dataclass.modeldata.DataSiswa
 import com.example.dataclass.uicontroller.route.DestinasiDetail
 import com.example.dataclass.viewmodel.DetailViewModel
+import com.example.dataclass.viewmodel.StatusUIDetail
 import com.example.dataclass.viewmodel.provider.PenyediaViewModel
 import kotlinx.coroutines.launch
 
-@OptIn
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
     navigateToEditItem: (Int) -> Unit,
@@ -39,10 +59,11 @@ fun DetailSiswaScreen(
             )
         },
         floatingActionButton = {
-            val uiState = viewModel.uiDetailState.collectAsState()
+            val uiState = viewModel.statusUIDetail
             FloatingActionButton(
                 onClick = {
-                    navigateToEditItem(uiState.value.detailSiswa.id)
+                    when(uiState) { is StatusUIDetail.Success ->
+                    navigateToEditItem(uiState.satusiswa.id) else->{}}
                 },
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
@@ -55,13 +76,12 @@ fun DetailSiswaScreen(
             }
         }, modifier = modifier
     ) { innerPadding ->
-        val uiState = viewModel.uiDetailState.collectAsState()
         val coroutineScope = rememberCoroutineScope()
         BodyDetailDataSiswa(
-            detailSiswaUiState = uiState.value,
+            statusUIDetail = viewModel.statusUIDetail,
             onDelete = {
                 coroutineScope.launch {
-                    viewModel.deleteSiswa()
+                    viewModel.hapusSatuSiswa()
                     navigateBack()
                 }
             },
@@ -71,3 +91,42 @@ fun DetailSiswaScreen(
         )
     }
 }
+
+@Composable
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        when(statusUIDetail){
+            is StatusUIDetail.Success -> DetailDataSiswa(
+                siswa = statusUIDetail.satusiswa,
+                modifier = Modifier.fillMaxWidth())
+            else -> {}
+        }
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
