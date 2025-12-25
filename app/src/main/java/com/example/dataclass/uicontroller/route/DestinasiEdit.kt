9ed83902cfab1791e5b3aff59f33c9c2f42c979e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-25 17:30:18 +0700

    feat: membuat Destinasi edit untuk route halaman edit

---
+++ app/src/main/java/com/example/dataclass/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,11 @@
+package com.example.dataclass.uicontroller.route
+
+import com.example.dataclass.R
+
+object DestinasiEdit : DestinasiNavigasi{
+    override val route = "item_edit"
+    override val titleRes =  R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
